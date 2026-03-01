a0dccbd68b762e02ef5a0e71115a64f13181d572
Shreyas <shreyasdumbre272@gmail.com>
2026-03-01 16:32:09 +0530

    Architect 1 development

--- Loop.py
+++ Loop.py
@@ -5,3 +5,11 @@ for i in arr:
         break
 else:
     print("Element Not Found")
+
+arr1 = [1,2,3,4,5]
+for i in arr:
+    if i == 3:
+        print("Element Found")
+        break
+else:
+    print("Element Not Found")
